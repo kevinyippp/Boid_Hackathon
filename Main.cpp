0cfcaeca7495d002bc223d7b3243ed5e541f2d6b
NhVNgn <nhanvyhl1234@gmail.com>
2024-07-13 13:15:25 -0700

    added sfml

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include "Header/DNS_Laws_Of_Physics.hpp"
-
+#include "SFML/Graphics.hpp"
 int main()
 {
 
 	DNS_PhysicsEngine::Vector2D myVector(10, 10);
 
-	std::cout << "x: " << myVector.x 
+	std::cout << "x: " << myVector.x;
+    sf::RenderWindow window(sf::VideoMode(800, 800), "Test");
+    sf::Event e;
+    while (window.isOpen()) {
+        while (window.pollEvent(e)) {
+            if (e.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+    return 0;
 }

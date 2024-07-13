24554afc116e6a3d893e6cd332b7b087835ce75a
NhVNgn <nhanvyhl1234@gmail.com>
2024-07-13 14:06:58 -0700

    dan choi so mot ha long

--- Main.cpp
+++ Main.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
 #include "Header/DNS_Laws_Of_Physics.hpp"
 #include "SFML/Graphics.hpp"
+#include "Game.h"
+
 int main()
 {
 
-	DNS_PhysicsEngine::Vector2D myVector(10, 10);
-
-	std::cout << "x: " << myVector.x;
-    sf::RenderWindow window(sf::VideoMode(800, 800), "Test");
-    sf::Event e;
-    while (window.isOpen()) {
-        while (window.pollEvent(e)) {
-            if (e.type == sf::Event::Closed)
-                window.close();
-        }
-    }
+    Game game;
+    game.Run();
     return 0;
 }
